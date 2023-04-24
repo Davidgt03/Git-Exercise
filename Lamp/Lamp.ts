c02875cea958e0a71fc1258d256b70b99922fb7b
david <daviidgt03@gmail.com>
2023-04-24 17:11:20 +0200

    commit

--- Lamp/Lamp.ts
+++ Lamp/Lamp.ts
@@ -1,8 +1,12 @@
 class Lamp {
   private isOn: boolean;
+  private hasBubble: boolean;
 
   public turnOn(): void {
     this.isOn = true;
+  }
+  public switch(): void {
+    this.isOn = !this.isOn;
   }
 
   public turnOff(): void {
